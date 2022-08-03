b4cc917ecf9c31e3ac32c492e8a658a718af9029
Ruben J Garcia <rubenjgarciab@gmail.com>
2022-08-03 10:46:06 +0200

    Add config from env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/abac'), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URI'),
+      }),
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.use(helmet());
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 
 bootstrap();
